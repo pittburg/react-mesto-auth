3d89f08075056ffda983fc0527b5d490a43de408
Andrey Aleksandrov <pittburg@yandex.ru>
2022-05-27 13:24:00 +0300

    Updates

---
+++ precache-manifest.75ad0ab94c4074674bf5bedabbf756b9.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41900ecc295ae579246fe34581fd67cd",
+    "url": "/react-mesto-auth/index.html"
+  },
+  {
+    "revision": "8a08dc530c809fa95ff4",
+    "url": "/react-mesto-auth/static/css/main.d20e5277.chunk.css"
+  },
+  {
+    "revision": "17ba9e6918d0ef029b21",
+    "url": "/react-mesto-auth/static/js/2.0a4a3c22.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/react-mesto-auth/static/js/2.0a4a3c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a6234ea07b75c95a490",
+    "url": "/react-mesto-auth/static/js/3.e61a5c96.chunk.js"
+  },
+  {
+    "revision": "8a08dc530c809fa95ff4",
+    "url": "/react-mesto-auth/static/js/main.ffd089c3.chunk.js"
+  },
+  {
+    "revision": "0ab8e8f85d95bdb6778a",
+    "url": "/react-mesto-auth/static/js/runtime-main.ededdefd.js"
+  },
+  {
+    "revision": "165563c4d60938a62db4e8e203419dcd",
+    "url": "/react-mesto-auth/static/media/Inter-Black.165563c4.woff"
+  },
+  {
+    "revision": "1b19574d075cd8d47035e3ffbb31e789",
+    "url": "/react-mesto-auth/static/media/Inter-Black.1b19574d.woff2"
+  },
+  {
+    "revision": "5f2ce7df2a2e8570f4c32a44414df347",
+    "url": "/react-mesto-auth/static/media/Inter-Black.5f2ce7df.ttf"
+  },
+  {
+    "revision": "5ff1f2a9a78730d7d0c309320ff3c9c7",
+    "url": "/react-mesto-auth/static/media/Inter-Medium.5ff1f2a9.ttf"
+  },
+  {
+    "revision": "99a3c0889316e1e78077fc60bc3d468a",
+    "url": "/react-mesto-auth/static/media/Inter-Medium.99a3c088.woff2"
+  },
+  {
+    "revision": "a9041df04dc9b9ce3b4c14e708d5a728",
+    "url": "/react-mesto-auth/static/media/Inter-Medium.a9041df0.woff"
+  },
+  {
+    "revision": "515cae74eee4925d56e6ac70c25fc0f6",
+    "url": "/react-mesto-auth/static/media/Inter-Regular.515cae74.ttf"
+  },
+  {
+    "revision": "678f9520fa54a52fb87f57e7efbabdef",
+    "url": "/react-mesto-auth/static/media/Inter-Regular.678f9520.woff"
+  },
+  {
+    "revision": "990ee43224d76dd50a936acaf489d0e4",
+    "url": "/react-mesto-auth/static/media/Inter-Regular.990ee432.woff2"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/react-mesto-auth/static/media/add_button.bdfb8ebb.svg"
+  },
+  {
+    "revision": "933106eaf93ffbb3807048cfeeecbcef",
+    "url": "/react-mesto-auth/static/media/black_heart.933106ea.svg"
+  },
+  {
+    "revision": "4c6a21695c10c9c77e7676ffa4a389c1",
+    "url": "/react-mesto-auth/static/media/close_icon.4c6a2169.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/react-mesto-auth/static/media/edit_button.58afbe73.svg"
+  },
+  {
+    "revision": "51772f7cae792434c441016d1f475dda",
+    "url": "/react-mesto-auth/static/media/logo.51772f7c.svg"
+  },
+  {
+    "revision": "34c4685038e8cf551566cb4ee96e20e5",
+    "url": "/react-mesto-auth/static/media/pencil.34c46850.svg"
+  },
+  {
+    "revision": "7fc4c149eaeefd2cf76f1e1b8ed62cb4",
+    "url": "/react-mesto-auth/static/media/success_image.7fc4c149.svg"
+  },
+  {
+    "revision": "48bb334e9ca31b6263593527bb6314ac",
+    "url": "/react-mesto-auth/static/media/trash.48bb334e.svg"
+  },
+  {
+    "revision": "c32299fa3125c41146aa8359b2307523",
+    "url": "/react-mesto-auth/static/media/unsuccess_image.c32299fa.svg"
+  },
+  {
+    "revision": "cb5f16439886c2d2781c07c597fe5b1a",
+    "url": "/react-mesto-auth/static/media/white_heart.cb5f1643.svg"
+  }
+]);
--- src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Footer() {
-  return (
-    <footer className="footer width-container">&copy; 2021 Mesto Russia</footer>
-  )
-}
-
-export default Footer;
